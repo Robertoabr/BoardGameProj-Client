c998df83a5b574c6c26a77f0070b5b17f0ff6e0e
Robertoabr <roberto.brillembourg@gmail.com>
2020-04-27 22:22:05 -0400

    added material ui react and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './pages/SignIn';
 
 function App() {
   useEffect(() => {
@@ -13,20 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignIn />
     </div>
   );
 }
